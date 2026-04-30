48e722065315304258c1ff41f8d08ba28387eb82
Maurice <mauricemuriuki15@gmail.com>
2026-04-30 12:27:05 +0300

    version 6

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from database import engine
 import models
-from routes import auth, products , admin , vendors, orders , ratings
-
-models.Base.metadata.create_all(bind=engine)
+from routes import auth, products, admin, vendors, orders, ratings
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://stupendous-praline-f80270.netlify.app"],
-    allow_credentials=True,
+    allow_origins=[
+        "https://stupendous-praline-f80270.netlify.app",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000"
+    ],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+models.Base.metadata.create_all(bind=engine)
+
 app.include_router(auth.router)
 app.include_router(products.router)
 app.include_router(admin.router)
-app.include_router(vendors.router) 
+app.include_router(vendors.router)
 app.include_router(orders.router)
 app.include_router(ratings.router)
 
